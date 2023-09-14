8a6a6958b4443bc12b2e77e2105aff3bb78a9264
moreiraggk <kaiquemoreira504@gmail.com>
2023-09-14 11:04:47 -0300

    novas tabelas usuarios e post

--- database/migrations/2023_09_14_133525_create_posts_table.php
+++ database/migrations/2023_09_14_133525_create_posts_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('usuario');
-            $table->string('foto');
-            $table->string('bio')->unique();
-            $table->timestamp('nome')->nullable();
-            $table->string('email');
-            $table->string('senha');
-            $table->rememberToken();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('mensagem');
             $table->timestamps();
         });
     }
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
